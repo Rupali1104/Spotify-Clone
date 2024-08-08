5fab1c00ed6318efdf1a8b26b368dc5a09de095b
Rupali R <rupaliii739@gmail.com>
2024-08-08 23:26:57 +0530

    volume button updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let currentSong = new Audio();
+let songs;
 
 function secondsToMinutesSeconds(seconds) {
     if (isNaN(seconds) || seconds < 0) {
@@ -52,7 +53,7 @@ async function main() {
 
 
     // get list of all songs
-    let songs = await getSongs();
+    songs = await getSongs();
     playMusic(songs[0], true)
 
     // show all the songs in the playlist
@@ -114,6 +115,34 @@ async function main() {
         document.querySelector(".left").style.left = "-120%"
     })
 
+    // add an event listener to previous
+    previous.addEventListener("click", () => {
+        currentSong.pause()
+        console.log("Previous clicked")
+        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+        if ((index - 1) >= 0) {
+            playMusic(songs[index - 1])
+        }
+    })
+
+    // add an event listener to next
+    next.addEventListener("click", () => {
+        currentSong.pause()
+        console.log("Next clicked")
+        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
+        if ((index + 1) < songs.length) {
+            playMusic(songs[index + 1])
+        }
+    })
+
+    // add an event to volume
+    document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
+        console.log("Setting volume to", e.target.value, "/ 100")
+        currentSong.volume = parseInt(e.target.value) / 100
+        if (currentSong.volume >0){
+            document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg", "volume.svg")
+        }
+    })
 
 }
 
